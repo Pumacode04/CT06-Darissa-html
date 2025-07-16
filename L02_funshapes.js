44b0ca324081aa97c68a425de09e894e5a16a1dd
Pumacode04 <cransonlim@yahoo.com.sg>
2025-07-16 08:11:13 +0000

    Jul 16, 2025, 8:11 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -3,7 +3,7 @@ Task 1: Add the setup() function
 */
 function setup(){
   createCanvas(600, 400)
-  background(235)
+  background(249)
 }
 function draw() {
   // Challenge 1: An eye using ellipse() function
