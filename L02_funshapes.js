43d757b8bf13431b7bc2828bc6dbed0215d80b3a
Pumacode04 <cransonlim@yahoo.com.sg>
2025-07-16 08:17:07 +0000

    Jul 16, 2025, 8:17 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -7,7 +7,7 @@ function setup(){
 }
 function draw() {
   // Challenge 1: An eye using ellipse() function
-Fill("Purple");
+Fill("Lemon");
 ellipse(50, 50, 50, 50);
   // Challenge 2: A face using circle() function
 
