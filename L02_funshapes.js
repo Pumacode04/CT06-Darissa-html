bdb7cf44d39da3084c724654165cfc917c63bf27
Pumacode04 <cransonlim@yahoo.com.sg>
2025-07-16 08:21:18 +0000

    Jul 16, 2025, 8:21 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -11,7 +11,7 @@ fill("Lavender");
 ellipse(300, 200, 150, 150);
 // ellipse(x, y, width, height)
 // circle(x, y, diameter)
-//rect
+// rect(x, y, width, height)
   // Challenge 2: A face using circle() function
 
   // Challenge 3: A house using rect() function
