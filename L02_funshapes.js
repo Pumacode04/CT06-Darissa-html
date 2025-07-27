b0fdfd402fe5890ddeff9a6bc2503251b6834849
Pumacode04 <cransonlim@yahoo.com.sg>
2025-07-27 08:34:38 +0000

    Jul 27, 2025, 8:34 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -32,7 +32,7 @@ function draw() {
   // Challenge 5: A square face using quad() function
 fill("Lavender");
 quad(20, 100, 170, 50, 170, 150, 20, 150);
-quad(400, 100, 300, 50, 500, 50, )
+quad(400, 100, 300, 50, 500, 50, 400, 200);
 
   // Challenge 6: Add some colours to the drawings in the above challenges
 
