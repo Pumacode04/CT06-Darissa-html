24ee83b5030688cb31c429cb54b70e139ac7306c
Pumacode04 <cransonlim@yahoo.com.sg>
2025-07-27 09:10:52 +0000

    Jul 27, 2025, 9:10 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -16,7 +16,7 @@ function draw() {
   // Task 1: Colour Gradient
 let spacing = 50
 for(i=0; i<5; i++) {
-  fill(i*50);
+  fill(i*spacing);
     circle(50 + i*50, 200, 40);
 }
   // Task 2: Colour Loop
