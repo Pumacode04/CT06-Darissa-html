0920c423c0dbb3e33c397a3c3ec59f645699c9a4
Pumacode04 <cransonlim@yahoo.com.sg>
2025-07-30 08:40:10 +0000

    Jul 30, 2025, 8:40 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -14,7 +14,7 @@ function draw() {
     circle(300, 200, diameter);
     noStroke();
     fill(50, 50);
-    circle(mouseX, mouseY, mouseZ)
+    circle(mouseX, mouseY, diameter)
 }
 
 function mousePressed() {
