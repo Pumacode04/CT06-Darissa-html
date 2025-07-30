ae14cd2908894ccf3739a0794fc61cb5fe3e34dd
Pumacode04 <cransonlim@yahoo.com.sg>
2025-07-30 08:09:02 +0000

    Jul 30, 2025, 8:09 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -39,8 +39,9 @@ function draw() {
   // Task 3: Row of Circles
 fill(255, 255, 255);
 rect(0, 50, 200, 100);
-noStroke
-fill(255, 0, 0)
+noStroke();
+fill(255, 0, 0);
+
   // Task 4: Grid of Circles
 //let x = 50;
 //for(let i=0; i<5; i++){
