c527f2c9bbcd63828a90dc700f175056b7950653
Pumacode04 <cransonlim@yahoo.com.sg>
2025-07-30 08:34:15 +0000

    Jul 30, 2025, 8:34 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -15,7 +15,7 @@ function draw() {
 }
 
 function mousePressed() {
-    shapeColor = color( random(255), random(255), random(255));
+//    shapeColor = color( random(255), random(255), random(255));
     diameter = diameter + 10;
     if (diameter > 500) diameter = 100;
 }
