9061a7b417d4c4c2ef409d62908182c280822fa4
Pumacode04 <cransonlim@yahoo.com.sg>
2025-08-06 07:40:07 +0000

    Aug 6, 2025, 7:40 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -36,6 +36,6 @@ function mouseReleased() {
 function mouseMoved() {
 //#    noStroke();
 //#    shapeColor = color( random(255), random(255), random(255));
-    fill(shapeColor);
-    ellipse(mouseX, mouseY, 10, 10);
+//#    fill(shapeColor);
+//    ellipse(mouseX, mouseY, 10, 10);
 }
