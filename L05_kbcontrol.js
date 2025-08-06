860f4b283462c6f54a6ecc5d78aa23f2dccfcf98
Pumacode04 <cransonlim@yahoo.com.sg>
2025-08-06 08:43:23 +0000

    Aug 6, 2025, 8:43 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -13,7 +13,7 @@ function draw() {
 //!    background(200);
 //!    rect(300, 200, rectSize, rectSize);
     background(200);
-    fill(random)
+    fill(random(255))
     if (key === 'c') {
         circle(100, 100, 200)
     }
