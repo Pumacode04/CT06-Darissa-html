4d50d9e39fc2c2e4b3de6a68f8952109f7e9f915
Pumacode04 <cransonlim@yahoo.com.sg>
2025-08-20 08:03:16 +0000

    Aug 20, 2025, 8:03 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -29,4 +29,6 @@ if (keyIsDown( DOWN_ARROW )) {
 if (keyIsDown( UP_ARROW )) {
     ypos = ypos - 1;
 }
+xpos = constrain(xpos, diameter/2, width - diameter/2);
+ypos = constrain(ypos, diameter/2, height - diameter/2);
 }
