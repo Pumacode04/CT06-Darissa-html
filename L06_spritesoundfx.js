dfc6b1795b68d436f504af85bbf0d3e0c0fab4ab
Pumacode04 <cransonlim@yahoo.com.sg>
2025-08-20 08:25:29 +0000

    Aug 20, 2025, 8:25 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -16,13 +16,12 @@ function setup() {
 function draw() {
 background("skyblue");
 image(staticImage, xpos, ypos, 110, 133);
-x
 //background(200)
 //noFill();
 //rect(50, 50, 300, 300);
 //fill(110, 0, 210);
 //rect(xpos, ypos, 50, 50);
-//f(keyIsDown( RIGHT_ARROW )) {
+//if(keyIsDown( RIGHT_ARROW )) {
 //    xpos = xpos + 1;
 //
 //if (keyIsDown( LEFT_ARROW )) {
