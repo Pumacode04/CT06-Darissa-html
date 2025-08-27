8643941c20f2c3b9d08eff9ab704f95132c9d1d8
Pumacode04 <cransonlim@yahoo.com.sg>
2025-08-27 09:02:41 +0000

    Aug 27, 2025, 9:02 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -23,7 +23,7 @@ background("skyblue");
 image(staticImage, xpos, ypos, 110, 133);
 image(secondguy, sgx, sgy, 96, 127);
 if(keyIsDown(65)) {
-    
+    gsy = gsy +
 }
 if(keyIsDown(68)) {
     gsx = gsx + 1
