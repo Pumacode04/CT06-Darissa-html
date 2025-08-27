603077c4d555e7b4a4caf53e300eee56294fc18e
Pumacode04 <cransonlim@yahoo.com.sg>
2025-08-27 08:52:47 +0000

    Aug 27, 2025, 8:52 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -8,6 +8,7 @@ function preload() {
     bgMusic = loadSound('assets/bossaNova.mp3')
 
     staticImage = loadImage('assets/pico-a.png')
+    secondguy = loadImage
 }
 function setup() {
     createCanvas(400, 400);
