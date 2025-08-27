c4e8b331c8c49a85cdf69d02a45d8d73e7bc2086
Pumacode04 <cransonlim@yahoo.com.sg>
2025-08-27 07:50:49 +0000

    Aug 27, 2025, 7:50 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -9,6 +9,6 @@ fill(0)
 stroke(255, 0, 0);
 strokeWeight(10);
 circle(150, height/2, 75);
-triangle(250, 250, 300, 175, 350, 300);
+triangle(250, 250, 300, 175, 350, 250);
 rect(350, height/2, 50, 50);
 }
