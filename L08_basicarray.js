d03ed32c9cb1734b5047902d6f444fe01e4d95d4
Pumacode04 <cransonlim@yahoo.com.sg>
2025-09-10 08:02:54 +0000

    Sep 10, 2025, 8:02 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,7 +1,7 @@
 // write your codes here
-let xpos = 150;
-let ypos = 250;
-let speed = 2;
+// let xpos = 150;
+// let ypos = 250;
+// let speed = 2;
 function setup() {
     createCanvas(400, 400);
     background(200);
