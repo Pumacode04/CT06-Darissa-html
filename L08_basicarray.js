8249d9a1ca19974f06f9dabb03a9d6bcd442a752
Pumacode04 <cransonlim@yahoo.com.sg>
2025-09-10 08:40:53 +0000

    Sep 10, 2025, 8:40 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -22,7 +22,8 @@ function setup() {
 //        text( "I like to eat " + favFood[index] + ".", 80, ypos)
 //        ypos = ypos + 50
         text("My Favourtie foods :", 50, 50);
-        text(i+1 + ")" + favFood[index], 50,)
+        text(i+1 + ")" + favFood[index], 50, ypos)
+        
     }
 }
 
