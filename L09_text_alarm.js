bd840f94278cf7f8500f0052e238757a8a6d37db
Pumacode04 <cransonlim@yahoo.com.sg>
2025-09-17 08:34:49 +0000

    Sep 17, 2025, 8:34 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,11 +1,16 @@
 // write your codes here
 let speed = 0.87
+let xpos = 300
+let ypos = 200
+let counter = 1
 function setup() {
     createCanvas(600, 400);
     background(200);
 }
 
 function draw() {
-    text("Bounce!", 300 - speed, 200);
-    speed = speed * -1
+    text("Bounce!", xpos - speed * 1, 200);
+    speed = speed * -1;
+    counter = counter + 1;
+    if ( xpos < 0 ) {}
 }
