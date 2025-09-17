45a6df15fca8841085bbf925e4852999aa1b848e
Pumacode04 <cransonlim@yahoo.com.sg>
2025-09-17 08:50:20 +0000

    Sep 17, 2025, 8:50 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,21 +1,21 @@
 // write your codes here
-let speed;
-let xpos;
-function setup() {
-    createCanvas(600, 400);
-    background(200);
-    speed = 1.87;
-    xpos = width/2;
-}
+// let speed;
+// let xpos;
+// function setup() {
+//     createCanvas(600, 400);
+//     background(200);
+//     speed = 1.87;
+//     xpos = width/2;
+// }
 
-function draw() {
-    background(220);
-    textSize(28);
-    translate( xpos, height/2);
-    textAlign(CENTER, CENTER);
-    text("Bounce!", 0, 0);
-    xpos = xpos + speed;
-    if ( xpos < 0 || xpos > 600 ) {
-        speed = speed * -1;
-     }
-}
+// function draw() {
+//     background(220);
+//     textSize(28);
+//     translate( xpos, height/2);
+//     textAlign(CENTER, CENTER);
+//     text("Bounce!", 0, 0);
+//     xpos = xpos + speed;
+//     if ( xpos < 0 || xpos > 600 ) {
+//         speed = speed * -1;
+//      }
+// }
