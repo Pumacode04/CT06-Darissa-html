90b590d78bfef35e6b5bde3775cd4e0554c8e816
Pumacode04 <cransonlim@yahoo.com.sg>
2025-09-17 08:46:44 +0000

    Sep 17, 2025, 8:46 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -14,10 +14,10 @@ function draw() {
     textAlign(CENTER, CENTER);
     background(200);
     text("Bounce!", 0, 0);
-    if ( xpos < 0 ) {
-        speed = speed * -1;
-    }
-    if ( xpos > 600 ) {
-        speed = speed * -1;
-    }
+    // if ( xpos < 0 ) {
+    //     speed = speed * -1;
+    // }
+    // if ( xpos > 600 ) {
+    //     speed = speed * -1;
+    // }
 }
