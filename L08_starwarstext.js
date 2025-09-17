a1c8dc70c092496ba0ad6ba2fff81eaac47fb466
Pumacode04 <cransonlim@yahoo.com.sg>
2025-09-17 07:34:08 +0000

    Sep 17, 2025, 7:34 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -15,7 +15,7 @@ function setup() {
 }
 
 function draw() {
-    
+    background(0);
     translate( width/2, ypos );
     text("the beginning of the star wars story...", 0, 0)
     if (keyIsDown(32) ) {
