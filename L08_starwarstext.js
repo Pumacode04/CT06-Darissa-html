d759eb8ac940cfa0795ae54d3381f0562325406b
Pumacode04 <cransonlim@yahoo.com.sg>
2025-09-17 07:42:08 +0000

    Sep 17, 2025, 7:42 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -21,7 +21,7 @@ function draw() {
     textAlign(CENTER, CENTER);
     text("the beginning of the star wars story...", 0, 0);
     text("AT EPISODE 4 IN THE YEAR 1995", 0, 50);
-    ypos = ypos - 1;
+    ypos = ypos - 0.6;
     if (keyIsDown(32) ) {
         soundsFX.play();
     }
