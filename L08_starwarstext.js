dbb46ef891b2671e459ab2c3b1de0d9d427e7ff6
Pumacode04 <cransonlim@yahoo.com.sg>
2025-09-17 07:40:29 +0000

    Sep 17, 2025, 7:40 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -9,7 +9,7 @@ function preload () {
 function setup() {
     createCanvas(600, 400);
     background(0);
-    sounds.FX();
+    soundsFX.loop();
     fill("cyan");
     textSize(48);
     ypos = height;
