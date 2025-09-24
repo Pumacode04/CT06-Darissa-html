36e961c0c04012b827841bbe6ab12b2e563b373f
Pumacode04 <cransonlim@yahoo.com.sg>
2025-09-24 08:27:51 +0000

    Sep 24, 2025, 8:27 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -71,7 +71,7 @@ function draw() {
         if (keyCode === UP_ARROW) {
             bgColor = color( random(0, 255), random(0, 255), random(0, 255));
         }
-        else if (keyCode === 49){ //
+        else if (keyCode === 49){ // keyCode 49 is 1
             counter--; // == counter = counter - 1
         }
     }
