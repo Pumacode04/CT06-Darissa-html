e982f77989e7467fe2b91a6572f1e124d2662ab6
Pumacode04 <cransonlim@yahoo.com.sg>
2025-09-24 07:49:55 +0000

    Sep 24, 2025, 7:49 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -42,10 +42,11 @@ function draw() {
     textSize(48);
     text( timeNow, width/2, height/2);
 
+    textSiz
     let counter = 5;
     counter = nf(counter, 2, 1);
     text("Counterdown Timer", width/2, height/2+ 50);
-    
+
 
 //    let pi = 3.14159267189
 //    pi = nf(pi, 2, 4); // 4 decimal place
