40daa199ca65692db6635f8c321ff015ed4bf323
Pumacode04 <cransonlim@yahoo.com.sg>
2025-09-24 07:47:55 +0000

    Sep 24, 2025, 7:47 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -42,7 +42,7 @@ function draw() {
     textSize(48);
     text( timeNow, width/2, height/2);
 
-        let 
+        let counter = 5
 //    let pi = 3.14159267189
 //    pi = nf(pi, 2, 4); // 4 decimal place
 //    fill(255, 0, 0);
