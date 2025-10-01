90736211101d682bb54bd52c3b5d36aaa2cd3d0b
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-01 08:51:20 +0000

    Oct 1, 2025, 8:51 AM

--- L10_name.js
+++ L10_name.js
@@ -33,7 +33,7 @@ function draw() {
     textSize(32);
     textAlign(CENTER, CENTER);
     text(username, width/2, 150);
-    text()
+    text(age, width/2, 170);
 
     textAlign(RIGHT, CENTER);
     textSize(18);
