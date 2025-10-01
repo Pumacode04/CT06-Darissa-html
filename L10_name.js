fb1334566fe1385967d6f5369e30cb8ede97c776
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-01 08:27:39 +0000

    Oct 1, 2025, 8:27 AM

--- L10_name.js
+++ L10_name.js
@@ -8,7 +8,7 @@ function setup() {
     createCanvas(600, 600);
     background(bgColor);
 
-    colorPicker = createColorPicker();
+    colorPicker = createColorPicker(bgColor);
     colorPicker.position( width/2-25, height/2);
 
     userInput= creatInput()
