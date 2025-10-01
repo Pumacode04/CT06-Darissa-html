a340a589aa4d66075560bb7ddc24cc503d467db7
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-01 08:25:58 +0000

    Oct 1, 2025, 8:25 AM

--- L10_name.js
+++ L10_name.js
@@ -11,7 +11,7 @@ function setup() {
     colorPicker = createColorPicker();
     colorPicker.position( width/2-25, height/2);
 
-    userInput.position
+    userInput.position( width/2-25, )
 }
 
 function draw() {
