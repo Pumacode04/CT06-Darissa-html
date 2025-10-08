2f4aedd97ebe1128461be1be9bdad662e435fc20
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-08 08:04:59 +0000

    Oct 8, 2025, 8:04 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -14,7 +14,7 @@ function setup() {
     button1 = createButton("Click on me lah");
     button1.position(50, 100);
     button1.size(100, 50);
-    button1.mousePressed(update)
+    button1.mousePressed(updateStory); //conent
 }
 
 function draw() {
