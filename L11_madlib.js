8c9003c9cfa5a9f4cb707e9de454dfec8a4855e4
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-08 08:37:35 +0000

    Oct 8, 2025, 8:37 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -39,7 +39,7 @@ function setup() {
     createCanvas(600, 600);
     background(224, 226, 255);
     
-    
+    textSize(14);
     textAlign(RIGHT, CENTER);
     text("Noun :", width/2, height/2);
 
