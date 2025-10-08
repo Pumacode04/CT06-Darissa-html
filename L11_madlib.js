12d0adb86ecab3a7502a25642980480d8aa6c165
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-08 08:42:21 +0000

    Oct 8, 2025, 8:42 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -49,7 +49,7 @@ function setup() {
     inputNoun = createInput(noun, "text");
     inputNoun.position(350, 50);
     
-    input
+    inputVerb = createInput
 
     button1 = createButton("Your kool sentence! :D");
     button1.position(50, 100);
