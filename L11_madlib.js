0b98675e780fd609a6661408aa81b64e5b29e7df
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-08 08:41:38 +0000

    Oct 8, 2025, 8:41 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -47,6 +47,7 @@ function setup() {
     inputNoun.position(350, 50);
     
     
+    
     button1 = createButton("Your kool sentence! :D");
     button1.position(50, 100);
     button1.size(100, 50);
