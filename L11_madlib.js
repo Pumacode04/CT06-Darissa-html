72cbbd77482b5ef5dc0d0a3525386a2e9f59a6f7
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-08 08:38:58 +0000

    Oct 8, 2025, 8:38 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -41,7 +41,7 @@ function setup() {
     
     textSize(24);
     textAlign(LEFT, CENTER);
-    text("Noun :", width/2-250, height/2);
+    text("Noun :", width/2-250, height/2-200);
 
     inputNoun = createInput(noun, "text");
     inputNoun.position(350, 50);
