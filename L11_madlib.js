dc746ae1b679870c677a8aa022e4c782964eded5
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-08 08:21:07 +0000

    Oct 8, 2025, 8:21 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -1,32 +1,32 @@
 // write your codes here
-let inputSomeText;
-let button1;
-let story = "Cat : Meow:3";
+// let inputSomeText;
+// let button1;
+// let story = "Cat : Meow:3";
 
-function setup() {
-    createCanvas(600, 400);
-    background(224, 226, 255);
+// function setup() {
+//     createCanvas(600, 400);
+//     background(224, 226, 255);
 
-    inputSomeText = createInput(story, "text");
-    inputSomeText.position(50, 50);
-//    inputSomeText.size(500, 100);
+//     inputSomeText = createInput(story, "text");
+//     inputSomeText.position(50, 50);
+// //    inputSomeText.size(500, 100);
 
-    button1 = createButton("Click on me lah");
-    button1.position(50, 100);
-    button1.size(100, 50);
-    button1.mousePressed(updateStory); //connect
+//     button1 = createButton("Click on me lah");
+//     button1.position(50, 100);
+//     button1.size(100, 50);
+//     button1.mousePressed(updateStory); //connect
 
-}
+// }
 
-function updateStory() {
-    background(224, 226, 255);
-    story = inputSomeText.value();
+// function updateStory() {
+//     background(224, 226, 255);
+//     story = inputSomeText.value();
 
-    textAlign(CENTER, CENTER);
-    textSize(28);
-    text(story, width/2, height/2);
-}
+//     textAlign(CENTER, CENTER);
+//     textSize(28);
+//     text(story, width/2, height/2);
+// }
 
-function draw() {
+// function draw() {
 
-}
+// }
