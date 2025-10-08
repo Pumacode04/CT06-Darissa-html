79190a8ded5d3253c332959044f5632381e95f5c
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-08 08:25:41 +0000

    Oct 8, 2025, 8:25 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -39,7 +39,7 @@ function setup() {
     createCanvas(600, 600);
     background(224, 226, 255);
 
-    inputNoun = createInput(story, "text")
+    inputNoun = createInput(noun, "text")
 }
 
 function updateStory() {
