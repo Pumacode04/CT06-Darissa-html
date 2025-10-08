528f9c1d359d3065d03031f4d1432941be1b793f
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-08 08:28:18 +0000

    Oct 8, 2025, 8:28 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -45,7 +45,8 @@ function setup() {
     button1 = createButton("Your kool sentence! :D");
     button1.position(50, 100);
     button1.size(100, 50);
-    button1.mousePressed
+    button1.mousePressed(updateStory);
+    
 }
 
 function updateStory() {
