2e8c0fe62267e90300a5f4b68671852b58ccd61d
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-08 08:36:43 +0000

    Oct 8, 2025, 8:36 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -39,7 +39,7 @@ function setup() {
     createCanvas(600, 600);
     background(224, 226, 255);
 
-    text("Noun :", )
+    text("Noun :", width/2, )
 
     inputNoun = createInput(noun, "text");
     inputNoun.position(350, 50);
