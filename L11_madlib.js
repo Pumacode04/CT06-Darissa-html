c536e80fcaeacb5c21849954d500da5d32c7e000
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-15 08:15:23 +0000

    Oct 15, 2025, 8:15 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -84,7 +84,7 @@ function setup() {
         "The {adjective} {noun} is {verb} {adverb} at the {place}",
         "One day, a {adjective} {noun} goes to the {place}",
         "Did you hear 'bout the {adjective} {noun} that tried to {verb} {adverb} near {place}",
-        "{adverb}, the {adjective} {noun} "
+        "{adverb}, the {adjective} {noun} started {"
     ];
 
     let selection = random(templates);
