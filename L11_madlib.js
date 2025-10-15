6df7b320a0404dfb99062e07149553938872b800
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-15 07:58:52 +0000

    Oct 15, 2025, 7:58 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -72,6 +72,9 @@ function setup() {
     inputAdverb = createInput(adverb, "text");
     inputAdverb.position(350, 200);
 
+    inputAdverb = createInput(adverb, "text");
+    inputAdverb.position(350, 200);
+
     button1 = createButton("Your kool sentence! :D");
     button1.position(50, 250);
     button1.size(100, 50);
