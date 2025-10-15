71c6fb35af09435a657b58512eeb7cb33e313372
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-15 08:12:47 +0000

    Oct 15, 2025, 8:12 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -90,7 +90,7 @@ function setup() {
     console.log("selection: " + selection); //+ string concatenation
     story = selection.replace("{adjective}", "tall");
     story = story.replace(  "{noun}",   "boy");
-    story = story.replace(  "{verb}",   "trugde ");
+    story = story.replace(  "{verb}",   "trugding ");
     story = story.replace(  "{adverb}",   "hesitantly");
     story = story.replace(  "{place}",   "pincipal's office");
 
