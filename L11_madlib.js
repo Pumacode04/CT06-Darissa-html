0b6b0069f5a5db72d7e105e43bd913a70fca8d35
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-15 08:00:02 +0000

    Oct 15, 2025, 8:00 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -83,7 +83,7 @@ function setup() {
     templates = [
         "The {adv} {noun} is {verb}{adverb} at the {place}",
         "One day, a {adj} {noun} goes to the {place}",
-        "Did you hear 'bout' ",
+        "Did you hear 'bout the {adj} {noun} that tried to {verb} ",
     ];
 }
 
