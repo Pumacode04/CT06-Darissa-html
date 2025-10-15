0b6c9f76c6e175203ca51a18e7bb0266e16ec20c
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-15 08:26:24 +0000

    Oct 15, 2025, 8:26 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -116,7 +116,7 @@ function updateStory() {
     story = story.replace(  "{noun}",   inputNoun.value());
     story = story.replace(  "{verb}",   inputVerb.value());
     story = story.replace(  "{adverb}",   inputAdverb.value());
-    story = story.replace(  "{place}",   "pincipal's office");
+    story = story.replace(  "{place}",   inputPlace.value());
     let selection = random(templates);
 }
 
