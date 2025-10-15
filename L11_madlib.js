829ce4cfd43f455cd67370e751be519f483a234f
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-15 08:05:01 +0000

    Oct 15, 2025, 8:05 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -88,6 +88,7 @@ function setup() {
 
     let selection = random(templates);
     console.log("selection: " + selection); //+ string concatenation
+    story = selection.replace
 }
 
 function updateStory() {
