7cb731d38f6f535e3537ec9d283de17e66bafe08
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-15 07:43:14 +0000

    Oct 15, 2025, 7:43 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -96,5 +96,6 @@ function draw() {
     textAlign(LEFT, CENTER);
     text("Verb :", width/2-250, height/2-187);
 
-    text("Adverb :", width/2-250, height/2-1)
+    text("Adverb :", width/2-250, height/2-137);
+    text
 }
